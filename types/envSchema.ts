1bcd766bcf9c97b8826bbfe6d1dc4a7feffbf131
gautchap <gautchap+github@outlook.com>
2024-06-15 18:09:44 +0200

    feat : add new env variables

--- types/envSchema.ts
+++ types/envSchema.ts
@@ -2,12 +2,18 @@ import z from "zod";
 
 const envSchema = z.object({
     AUTH_SECRET: z.string().trim().min(1),
+    DATABASE_URL: z.string().trim().min(1),
+    AUTH_GITHUB_SECRET: z.string().trim().min(1),
+    AUTH_GITHUB_ID: z.string().trim().min(1),
     PORT: z.number().default(3000),
     NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 });
 
 const envServer = envSchema.safeParse({
-    AUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
+    AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
 });
